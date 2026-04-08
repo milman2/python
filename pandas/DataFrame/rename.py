58a66e681fd186dc4ac361bd02ce5625c36e4583
milman2 <milman2@hanmail.net>
2026-04-08 22:38:59 +0900

    add examples for renaming row indices and column names in pandas DataFrames

---
+++ pandas/DataFrame/rename.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+df = pd.DataFrame([
+  [15, '남', '덕영중'],
+  [17, '여', '수리중']
+  ],
+  index=['서준', '예은'],
+  columns=['나이', '성별', '학교'])
+
+print(df)
+print(df.index)
+print(df.columns)
+
+df = df.rename(columns={'나이' : '연령', '성별' : '남녀', '학교' : '소속'})
+print(df)
+df = df.rename(index={'서준' : '학생1', '예은' : '학생2'})
+print(df)
+
+# row index, column name 변경
+df.index = ['Student1', 'Student2']
+df.columns = ['Age', 'Sex', 'School']
+print(df)
+print(df.index)
+print(df.columns)
--- pandas/test-DataFrame.py
+++ pandas/test-DataFrame.py
@@ -1,30 +1,7 @@
 import pandas as pd
 
 # DataFrame/Construction.py
-
-# row index / column name
-df = pd.DataFrame([
-  [15, '남', '덕영중'],
-  [17, '여', '수리중']
-  ],
-  index=['서준', '예은'],
-  columns=['나이', '성별', '학교'])
-print(df)
-print(df.index)
-print(df.columns)
-
-# row index, column name 변경
-df.index = ['학생1', '학생2']
-df.columns = ['연령', '남녀', '소속']
-print(df)
-print(df.index)
-print(df.columns)
-
-df = df.rename(columns={'연령': '나이', '남녀': '성별', '소속': '학교'})
-print(df)
-df = df.rename(index={'학생1': '서준', '학생2': '예은'})
-print(df)
-
+# DataFrame/rename.py
 # DataFrame/drop.py
 # DataFrame/SelectRow.py
 # DataFrame/SelectColumn.py
